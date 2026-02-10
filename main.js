b7301b59cd026a0a1a51f0cb44d38b68d0e96f12
Süle Árpád <153063152+arpi0@users.noreply.github.com>
2026-02-10 09:40:21 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 function logalert()
 {
-    alert("Czigány");
+
+
+var resp = window.prompt("Login to your account", "Username");
+var pass = window.prompt("Password", "Password");
+document.getElementById("demo").innerText = resp;
+
 }

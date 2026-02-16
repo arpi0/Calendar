75cccb811bf27045a9939fdd871cb204e9892654
Süle Árpád <153063152+arpi0@users.noreply.github.com>
2026-02-16 11:30:51 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
+let loggedin = false;
 function logalert()
 {
 
+if ( loggedin== false)
+    {
+        var resp = window.prompt("Login to your account", "Username");
+        var pass = window.prompt("Password", "Password");
+        document.getElementById("demo").innerText = resp;
+        loggedin= true;
+    }
+else 
+    {
+        alert("already signed in");
+    }
+}
 
-var resp = window.prompt("Login to your account", "Username");
-var pass = window.prompt("Password", "Password");
-document.getElementById("demo").innerText = resp;
+function newdate() 
+{
+if(loggedin==true)
+    {
+        var name = window.prompt("Name of your event");
+        var date = window.prompt("Date of your event");
 
+        document.getElementById(date).innerText = name;
+    }
+else 
+    {
+        alert("Please sign in to make an event");
+    }
 }
